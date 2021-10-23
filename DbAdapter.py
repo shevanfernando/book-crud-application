ce9cb349a26dd1a8c7a0cac8dfc6b8cb5bf05954
Shevan Fernando <w.k.b.s.t.fernando@gmail.com>
2021-10-23 14:03:30 +0530

    mysql connector add

---
+++ DbAdapter.py
@@ -0,0 +1,3 @@
+import mysql.connector as mysql
+
+db = mysql.connect(host="localhost", username="root", password="")
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-
+import DbAdapter as DB
 
 if __name__ == '__main__':
     name = "Book-CRUD"
     print(f'{name} Application Start...')
+
+    try:
+        DB
+    except:
+        print('DB is not connected.')
